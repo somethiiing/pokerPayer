31b56a2d5dc7b163a428da939052a4a48bbcd4df
kovictor <victorchunfungko@gmail.com>
2022-06-24 21:38:55 -0700

    initial commit on greedy approach of payments
    created a function that creates payment objects
    created a function that returns a list of payments required to finish the game

--- server/utils/utils.js
+++ server/utils/utils.js
@@ -80,6 +80,61 @@ const addTransactionToRoom = ({room, player, amount, transactionType, isCash}) =
   return room;
 }
 
+const createPayment = ({sender, receiver, payment, isCash}) => {
+  //add a log here for each payment being created
+  return {
+    sender,
+    receiver,
+    payment,
+    isCash
+  }
+}
+
+const getListofPayments = ({players}) => {
+  let payments = [];
+  let receivers = [];
+  let senders = [];
+
+  // iterate through players list, sort by receivers (positive balance) and senders (negative balance, senders will have +ve number in their end balance afterwards)
+  for(const player of players) {
+    if (player.endBalance < 0) {
+      player.endBalance *= -1;
+      senders.push(player);
+    } else {
+      receivers.push(player);
+    }
+  }
+  // sort senders and receivers list from greatest to smallest
+  senders.sort(func(a,b){return a.endBalance > b.endBalance});
+  receivers.sort(func(a,b){return a.endBalance > b.endBalance});
+
+  var receiver = receivers.pop();
+  var sender = senders.pop();
+  while(sender != null) {
+    if(sender.endBalance < receiver.endBalance) {
+      payments.push(createPayment(sender.displayName, receiver.displayName, sender.endBalance, false));
+      receiver.endBalance = receiver.endBalance - sender.endBalance;
+      if (senders.length > 0) {
+        sender = senders.pop();
+      }
+    } else if (sender.endBalance > receiver.endBalance) {
+      payments.push(createPayment(sender.displayName, receiver.displayName, receiver.endBalance, false));
+      sender.endBalance = sender.endBalance - receiver.endBalance
+      receiver = receivers.pop();
+    } else {  //sender and receiver balance are equal
+      payments.push(createPayment(sender.displayName, receiver.displayName, receiver.endBalance, false));
+      if (senders.length == 0) {
+        sender = null
+      } else {
+        sender = senders.pop()
+        receiver = receivers.pop()
+      }
+    }
+  }
+
+  return payments;
+}
+
 module.exports = {
   generateNewTimestamp,
   generateNewRoomData,
